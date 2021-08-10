fe6a3303069b0ad73aca96668882ad87b8cf13b2
ismail harmanda <iharmanda@hotmail.com>
2021-08-10 16:05:32 +0300

    import components to Meals.js and blend them

---
+++ src/Components/Meals/Meals.js
@@ -0,0 +1,12 @@
+import MealsSummary from "./MealsSummary";
+import AvailableMeals from "./AvailableMeals";
+const Meals = () => {
+  return (
+    <>
+      <MealsSummary />
+      <AvailableMeals />
+    </>
+  );
+};
+
+export default Meals;
