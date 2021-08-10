9bb7a7a4e376075670e137a00523e97730ec2370
ismail harmanda <iharmanda@hotmail.com>
2021-08-10 18:19:32 +0300

    implement css in input.js

--- src/Components/UI/Input.js
+++ src/Components/UI/Input.js
@@ -1,6 +1,7 @@
+import classes from "./Input.module.css";
 const Input = (props) => {
   return (
-    <div>
+    <div className={classes.input}>
       <label htmlFor={props.input.id}>{props.label}</label>
       <input {...props.input} />
     </div>
