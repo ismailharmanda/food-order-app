ec79bced19298d089d36f141d8c494761f42c9be
ismail harmanda <iharmanda@hotmail.com>
2021-08-10 15:52:07 +0300

    create AvailableMeals.js within DUMMY_MEALS

--- src/Components/Meals/MealsSummary.js
+++ src/Components/Meals/MealsSummary.js
@@ -1,6 +1,8 @@
+import classes from "./MealsSummary.module.css";
+
 const MealsSummary = () => {
   return (
-    <section>
+    <section className={classes.summary}>
       <h2>Delicious Food, Delivered To You</h2>
       <p>
         Choose your favorite meal from our broad selection of available meals
