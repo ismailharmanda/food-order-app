60a885020151e3bec0ad4e239f02fb990a999518
ismail harmanda <iharmanda@hotmail.com>
2021-08-10 17:40:57 +0300

    create MealItem

---
+++ src/Components/Meals/MealItem/MealItem.js
@@ -0,0 +1,15 @@
+const MealItem = (props) => {
+  const price = `$${props.price.toFixed(2)}`;
+  return (
+    <li>
+      <div>
+        <h3>{props.name}</h3>
+        <div>{props.description}</div>
+        <div>{price}</div>
+      </div>
+      <div></div>
+    </li>
+  );
+};
+
+export default MealItem;
