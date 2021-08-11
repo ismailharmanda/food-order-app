4ea962b8951c1f7ca274cf42a5cebbaa35493da6
ismail harmanda <iharmanda@hotmail.com>
2021-08-11 16:51:44 +0300

    All components in App.js needs access to context's data. Wrap them all

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Header from "./Components/Layout/Header";
 import Meals from "./Components/Meals/Meals";
 import Cart from "./Components/Cart/Cart";
+import CartProvider from "./store/CartProvider";
 
 const App = () => {
   const [displayCart, setDisplayCart] = useState(false);
@@ -12,13 +13,13 @@ const App = () => {
     setDisplayCart(false);
   };
   return (
-    <>
+    <CartProvider>
       {displayCart && <Cart onClose={hideCartHandler} />}
       <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
-    </>
+    </CartProvider>
   );
 };
 
